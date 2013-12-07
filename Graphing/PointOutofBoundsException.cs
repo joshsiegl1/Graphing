b88a8fc62feba7cd6897b489ecd6abb3ad7fe551
Josh <joshsiegl@gmail.com>
2013-12-06 22:15:32 -0500

    Add recursive OrderX function to Graph.cs, rename PointSequenceInvalidExcpetion to PointOutofBoundsException, accept points of any X value and then order them from smallest to largest, refactor Plot(List<Vector2> points) function to order X values using OrderX

---
+++ Graphing/PointOutofBoundsException.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Graphing
+{
+    internal class PointOutofBoundsException : System.Exception
+    {
+        public PointOutofBoundsException(string message = 
+            "A point is out of bounds from the graph") : base(message) { } 
+    }
+}
